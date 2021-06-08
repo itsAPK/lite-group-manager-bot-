740cff65aab2037054bb63811334136df6cf7634
Apoorva Kumar <apkvandagadde2@gmail.com>
2021-06-08 15:14:17 +0530

    Initial Commit

--- bot/config.py
+++ bot/config.py
@@ -1,1 +1,21 @@
-. 
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+
+class Config:
+    LOGGER = True
+    STRING_SESSION = os.environ.get("STRING_SESSION")
+    APP_ID = os.environ.get("API_ID")
+    API_HASH = os.environ.get("API_HASH")
+    DATABASE_URI = os.environ.get("DATABASE_URI")
+    VERSION = os.environ.get("VERSION")
+    WORKERS = int(os.environ.get("WORKERS", 16))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -1001410504549))
+    SUDO_USERS=os.environ.get("SUDO_USERS")
+    PREFIX_HANDLER = os.environ.get("PREFIX_HANDLER", "/").split()
+    BOT_TOKEN=os.environ.get("BOT_TOKEN")
+   
